65e8beaf3ecdffee6611767d21f78cbc9e73ba8a
鴇峰 朔華 <sakuhana@tsuitachi.net>
2024-12-03 18:05:24 +0900

    enhance(frontend): ユーザーページで常にチャンネル投稿が含まれるように

--- packages/frontend/src/pages/user/index.timeline.vue
+++ packages/frontend/src/pages/user/index.timeline.vue
@@ -47,7 +47,7 @@ const pagination = computed(() => tab.value === 'featured' ? {
 		userId: props.user.id,
 		withRenotes: tab.value === 'all',
 		withReplies: tab.value === 'all',
-		withChannelNotes: tab.value === 'all',
+		withChannelNotes: true,
 		withFiles: tab.value === 'files',
 		includeSensitiveChannel: $i != null,
 	},
